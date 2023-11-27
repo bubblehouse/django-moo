7ea1e6d5c0ab55ca6f7440690c86abdaf74699dd
Phil Christensen <phil@bubblehouse.org>
2023-11-26 21:37:21 -0500

    refactor: split up model files

---
+++ termiverse/core/models/object.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+class Object(models.Model):
+    name = models.CharField(max_length=255)
+    unique_name = models.BooleanField(default=False)
+    owner = models.ForeignKey('self', related_name='+', blank=True, null=True, on_delete=models.SET_NULL,)
+    location = models.ForeignKey('self', related_name='contents', blank=True, null=True, on_delete=models.SET_NULL)
+    parents = models.ManyToManyField('self', related_name='children', blank=True, symmetrical=False, through='Relationship')
+    observers = models.ManyToManyField('self', related_name='observations', blank=True, symmetrical=False, through='Observation')
+
+    def __str__(self):
+        return "#%s (%s)" % (self.id, self.name)
+
+class Relationship(models.Model):
+    class Meta:
+        unique_together = [['child', 'parent']]
+
+    child = models.ForeignKey(Object, related_name='parent', on_delete=models.CASCADE)
+    parent = models.ForeignKey(Object, related_name='child', on_delete=models.CASCADE)
+    weight = models.IntegerField(default=0)
+
+class Observation(models.Model):
+    object = models.ForeignKey(Object, related_name='observer', on_delete=models.CASCADE)
+    observer = models.ForeignKey(Object, related_name='object', on_delete=models.CASCADE)
+
+class Alias(models.Model):
+    class Meta:
+        verbose_name_plural = 'aliases'
+
+    object = models.ForeignKey(Object, related_name='aliases', on_delete=models.CASCADE)
+    alias = models.CharField(max_length=255)
---
+++ termiverse/core/models/verb.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.core import validators
+
+from ..code import ExecutableVerbMixin
+
+class Verb(models.Model, ExecutableVerbMixin):
+    code = models.TextField(blank=True, null=True)
+    repo = models.ForeignKey("Repository", related_name='+', blank=True, null=True, on_delete=models.SET_NULL)
+    filename = models.CharField(max_length=255, blank=True, null=True)
+    ref = models.CharField(max_length=255, blank=True, null=True)
+    owner = models.ForeignKey("Object", related_name='+', blank=True, null=True, on_delete=models.SET_NULL)
+    origin = models.ForeignKey("Object", related_name='verbs', on_delete=models.CASCADE)
+    ability = models.BooleanField(default=False)
+    method = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "%s {#%s on %s}" % (
+            self.annotated(), self.id, self.origin
+        )
+
+    def annotated(self):
+        ability_decoration = ['', '@'][self.ability]
+        method_decoration = ['', '()'][self.method]
+        verb_name = self.name()
+        return ''.join([ability_decoration, verb_name, method_decoration])
+
+    def name(self):
+        return self.names.all()[0].name
+
+class VerbName(models.Model):
+    verb = models.ForeignKey(Verb, related_name='names', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return "%s {#%s on %s}" % (
+            self.name, self.verb.id, self.verb.origin
+        )
+
+# TODO: add support for additional URL types and connection details
+class URLField(models.CharField):
+    default_validators = [validators.URLValidator(schemes=['https'])]
+
+class Repository(models.Model):
+    slug = models.SlugField()
+    url = URLField(max_length=255)
+    prefix = models.CharField(max_length=255)
