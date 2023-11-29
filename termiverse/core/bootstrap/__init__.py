35b1fb10bb169f5ef4d10b3e01c26be1f6ef76fa
Phil Christensen <phil@bubblehouse.org>
2023-11-29 18:29:41 -0500

    fix: continuing to address init issues

--- termiverse/core/bootstrap/__init__.py
+++ termiverse/core/bootstrap/__init__.py
@@ -4,3 +4,9 @@ def get_source(filename, dataset='default'):
     verb_path = pkg.resource_filename('termiverse.core.bootstrap', '%s_verbs/%s' % (dataset, filename))
     with open(verb_path) as f:
         return f.read()
+
+def load_python(python_path):
+    """
+    Execute a provided Python bootstrap file against the provided database.
+    """
+    exec(compile(open(python_path).read(), python_path, 'exec'), globals(), dict())
