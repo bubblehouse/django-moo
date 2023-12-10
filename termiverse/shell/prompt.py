201608ee59d899aabce11f9426327ab3e260fd58
Phil Christensen <phil@bubblehouse.org>
2023-12-10 18:51:37 -0500

    fix: its okay to save the whole model object

--- termiverse/shell/prompt.py
+++ termiverse/shell/prompt.py
@@ -11,7 +11,7 @@ from ..core import code, models
 log = logging.getLogger(__name__)
 
 def embed(
-    user_id,
+    user: models.User,
     globals=None,
     locals=None,
     configure: Callable[[PythonRepl], None] | None = None,
@@ -49,7 +49,7 @@ def embed(
 
     # Create REPL.
     repl = CustomRepl(
-        user_id=user_id,
+        user=user,
         get_globals=get_globals,
         get_locals=get_locals,
         vi_mode=vi_mode,
@@ -66,8 +66,8 @@ def embed(
     return coroutine()  # type: ignore
 
 class CustomRepl(PythonRepl):
-    def __init__(self, user_id, *a, **kw):
-        self.user_id = user_id
+    def __init__(self, user, *a, **kw):
+        self.user = user
         super().__init__(*a, **kw)
 
     @sync_to_async
@@ -76,11 +76,10 @@ class CustomRepl(PythonRepl):
         Evaluate the line and print the result.
         """
         # does this need to be called from outside the synchronous function?
-        caller = models.User.objects.get(pk=self.user_id)
         # Try eval first
-        log.error(f"{caller}: {line}")
+        log.error(f"{self.user}: {line}")
         try:
-            result = code.r_eval(caller, line, self.get_locals())
+            result = code.r_eval(self.user, line, self.get_locals())
             self._store_eval_result(result)
             return result
         except SyntaxError:
@@ -88,5 +87,5 @@ class CustomRepl(PythonRepl):
         # If not a valid `eval` expression, compile as `exec` expression
         # but still run with eval to get an awaitable in case of a
         # awaitable expression.
-        result = code.r_exec(caller, line, self.get_locals())
+        result = code.r_exec(self.user, line, self.get_locals())
         return result
