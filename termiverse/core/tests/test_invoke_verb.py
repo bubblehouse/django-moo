9c9119198aaca5de2eb971a9fa2b05819f11aed3
Phil Christensen <phil.christensen@benevolent.ai>
2024-02-09 19:13:14 -0500

    refactor: test improvements

--- termiverse/core/tests/test_invoke_verb.py
+++ termiverse/core/tests/test_invoke_verb.py
@@ -1,65 +1,55 @@
+from termiverse.tests import *
 from .. import code
-from ..models import Object
-from . import termiverse_init
 
 import pytest
 
 @pytest.mark.django_db
-def test_dir(termiverse_init):
-    user = Object.objects.get(name='Wizard')
+def test_dir(t_init, t_wizard):
     def _writer(msg):
-        raise Exception("print called")
-    with code.context(user, _writer):
+        raise Exception("print was called unexpectedly")
+    with code.context(t_wizard, _writer):
         writer = code.get_output()
-        locals = {}
         globals = code.get_default_globals()
         globals.update(code.get_restricted_environment(writer))
-        result = code.do_eval("dir()", locals, globals)
+        result = code.do_eval("dir()", {}, globals)
         assert result == []
 
 @pytest.mark.django_db
-def test_print(termiverse_init):
+def test_print(t_init, t_wizard):
     printed = []
-    user = Object.objects.get(name='Wizard')
     def _writer(msg):
         printed.append(msg)
-    with code.context(user, _writer):
+    with code.context(t_wizard, _writer):
         writer = code.get_output()
-        locals = {}
         globals = code.get_default_globals()
         globals.update(code.get_restricted_environment(writer))
-        result = code.do_eval("print('test')", locals, globals)
+        result = code.do_eval("print('test')", {}, globals)
         assert result is None
         assert printed == ['test']
 
 @pytest.mark.django_db
-def test_caller_print(termiverse_init):
+def test_caller_print(t_init, t_wizard):
     printed = []
-    user = Object.objects.get(name='Wizard')
     def _writer(msg):
         printed.append(msg)
-    with code.context(user, _writer):
+    with code.context(t_wizard, _writer):
         writer = code.get_output()
-        locals = {}
         globals = code.get_default_globals()
         globals.update(code.get_restricted_environment(writer))
         src = "from termiverse.core import api\nprint(api.caller)"
-        code.r_exec(src, locals, globals)
+        code.r_exec(src, {}, globals)
         assert printed == ['#2 (Wizard)']
 
 @pytest.mark.django_db
-def test_caller_look(termiverse_init):
+def test_caller_look(t_init, t_wizard):
     printed = []
-    user = Object.objects.get(name='Wizard')
-    location = user.location
-    description = location.properties.get(name="description")
+    description = t_wizard.location.properties.get(name="description")
     def _writer(msg):
         printed.append(msg)
-    with code.context(user, _writer):
+    with code.context(t_wizard, _writer):
         writer = code.get_output()
-        locals = {}
         globals = code.get_default_globals()
         globals.update(code.get_restricted_environment(writer))
         src = "from termiverse.core import api\napi.caller.invoke_verb('look')"
-        code.r_exec(src, locals, globals)
+        code.r_exec(src, {}, globals)
         assert printed == [description.value]
