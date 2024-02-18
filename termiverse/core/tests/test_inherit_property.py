f5658df803a7e027c06e921b86328a6437c70604
Phil Christensen <phil.christensen@benevolent.ai>
2024-02-18 12:30:11 -0500

    test: additional tests prove inheritance is stil broken

--- termiverse/core/tests/test_inherit_property.py
+++ termiverse/core/tests/test_inherit_property.py
@@ -1,6 +1,5 @@
 from termiverse.tests import *
 from ..models import Object
-from ..models.property import AccessibleProperty
 
 import pytest
 
@@ -10,9 +9,23 @@ def test_parent_property(t_init: Object):
     parent_description = room_class.properties.get(name="description")
     room = Object.objects.create(name="new room")
     room.parents.add(room_class)
-
-    with pytest.raises(AccessibleProperty.DoesNotExist):
-        description = room.get_property(name="description", inherited=False)
-
     description = room.get_property(name="description")
     assert description == parent_description.value
+
+@pytest.mark.django_db
+def test_inherited_property_owner(t_init: Object):
+    player = Object.objects.get(name="Player")
+    room_class = Object.objects.get(name="room class")
+    room = Object.objects.create(name="new room", owner=player)
+    room.parents.add(room_class)
+    description = room.get_property(name="description", recurse=False, original=True)
+    assert description.origin == room
+
+@pytest.mark.django_db
+def test_inherited_property_origin(t_init: Object):
+    player = Object.objects.get(name="Player")
+    room_class = Object.objects.get(name="room class")
+    room = Object.objects.create(name="new room", owner=player)
+    room.parents.add(room_class)
+    description = room.get_property(name="description", recurse=False, original=True)
+    assert description.owner == player
