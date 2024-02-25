06d2240281c589e14feef7b4bce359218fa0f522
Phil Christensen <phil.christensen@benevolent.ai>
2024-02-25 18:02:17 -0500

    test: another parser test

--- termiverse/core/tests/test_parser.py
+++ termiverse/core/tests/test_parser.py
@@ -6,8 +6,17 @@ import pytest
 
 @pytest.mark.django_db
 def test_parse_look(t_init: Object, t_wizard: Object):
-    lex = parse.Lexer("look my bag of holding")
+    lex = parse.Lexer("look")
     parser = parse.Parser(lex, t_wizard)
     verb = parser.get_verb()
     names = [v['name'] for v in verb.names.values('name')]
     assert 'look' in names
+    assert parser.dobj is None
+
+@pytest.mark.django_db
+def test_parse_look_my_bag_of_holding(t_init: Object, t_wizard: Object):
+    bag = Object.objects.get(name="bag of holding")
+    lex = parse.Lexer("look my bag of holding")
+    parser = parse.Parser(lex, t_wizard)
+    verb = parser.get_verb()
+    assert parser.dobj == bag
