ad3dba7480d49ca0476b0aebe40e645c700a2d75
Phil Christensen <phil@bubblehouse.org>
2024-03-05 20:40:31 -0500

    fix: fixes for permissions and associated tests

--- termiverse/core/bootstrap/default_verbs/system_set_default_permissions.py
+++ termiverse/core/bootstrap/default_verbs/system_set_default_permissions.py
@@ -3,6 +3,7 @@ from termiverse.core import api
 obj = api.args[0]
 obj.allow('wizards', 'anything')
 obj.allow('owners', 'anything')
+obj.allow('everyone', 'read')
 
 if obj.kind == 'verb':
     obj.allow('everyone', 'execute')
