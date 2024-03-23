1185d846dda51c3c5e501478e6845b915823fb01
Phil Christensen <phil@bubblehouse.org>
2024-03-23 13:03:21 -0400

    test: added inheritane permissions tests

--- termiverse/core/tests/test_permissions.py
+++ termiverse/core/tests/test_permissions.py
@@ -1,7 +1,9 @@
 import pytest
 
 from termiverse.core.models import Object, Player
+from termiverse.core.models.object import AccessibleObject
 from termiverse.tests import *  # pylint: disable=wildcard-import
+from .. import code
 
 @pytest.mark.django_db
 def test_regular_user_can_read_a_thing(t_init, t_wizard):
@@ -61,3 +63,29 @@ def test_add_a_simple_deny_clase(t_init, t_wizard):
 
     assert user.is_allowed('read', thing)
     assert not user.is_allowed('write', thing)
+
+@pytest.mark.django_db
+def test_cant_create_child_of_an_object_that_isnt_yours(t_init, t_wizard):
+    printed = []
+    def _writer(msg):
+        printed.append(msg)
+    user = AccessibleObject.objects.get(name__iexact='player')
+    parent_thing = AccessibleObject.objects.create(name='parent thing', owner=t_wizard)
+    with code.context(user, _writer):
+        child_thing = AccessibleObject.objects.create(name='child thing', owner=user)
+        with pytest.raises(PermissionError) as excinfo:
+            child_thing.parents.add(parent_thing)
+        assert str(excinfo.value) == "#13 (Player) is not allowed derive on #14 (parent thing)"
+
+@pytest.mark.django_db
+def test_cant_create_parent_of_an_object_that_isnt_yours(t_init, t_wizard):
+    printed = []
+    def _writer(msg):
+        printed.append(msg)
+    user = AccessibleObject.objects.get(name__iexact='player')
+    child_thing = AccessibleObject.objects.create(name='child thing', owner=t_wizard)
+    with code.context(user, _writer):
+        parent_thing = AccessibleObject.objects.create(name='parent thing', owner=user)
+        with pytest.raises(PermissionError) as excinfo:
+            child_thing.parents.add(parent_thing)
+        assert str(excinfo.value) == "#13 (Player) is not allowed transmute on #14 (child thing)"
