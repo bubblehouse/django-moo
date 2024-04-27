c79f840b666375c6e52301153145984b845b6c66
Phil Christensen <phil.christensen@benevolent.ai>
2024-04-27 15:39:10 -0400

    refactor: simplify parsing by always storing a list of prepositions

--- moo/core/tests/test_lexer.py
+++ moo/core/tests/test_lexer.py
@@ -10,14 +10,14 @@ def test_lex_direct_object():
 
 def test_lex_object_of_the_preposition():
     lex = parse.Lexer("look at this")
-    assert lex.prepositions["at"][1] == "this"
+    assert lex.prepositions["at"][0][1] == "this"
 
 def test_lex_direct_object_with_preposition():
     lex = parse.Lexer("look at painting with the glasses")
-    assert lex.prepositions["at"][1] == "painting"
-    assert lex.prepositions["with"][0] == "the"
-    assert lex.prepositions["with"][1] == "glasses"
+    assert lex.prepositions["at"][0][1] == "painting"
+    assert lex.prepositions["with"][0][0] == "the"
+    assert lex.prepositions["with"][0][1] == "glasses"
 
 def test_lex_look_at_QUOTED_painting_with_the_glasses():
     lex = parse.Lexer("look at 'painting with the glasses'")
-    assert lex.prepositions["at"][1] == "painting with the glasses"
+    assert lex.prepositions["at"][0][1] == "painting with the glasses"
