1c8b09cad5f32f9c1432cd091edeac428a1a8a4a
Phil Christensen <phil.christensen@benevolent.ai>
2024-04-28 19:15:02 -0400

    fix: use existing hosts

--- moo/settings/dev.py
+++ moo/settings/dev.py
@@ -7,7 +7,7 @@ SECRET_KEY = "django-insecure-06xo%2f39ohlfrgu&)%1lsrlx6jqy)s%#_-$rc5l_z1+p6w(=r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['moo.dev.shacklyn.net', 'probe.cluster.local']
+ALLOWED_HOSTS = ['termiverse.dev.shacklyn.net', 'probe.cluster.local']
 CSRF_TRUSTED_ORIGINS = ['https://moo.dev.shacklyn.net', 'https://probe.cluster.local']
 
 # Database
@@ -16,10 +16,10 @@ CSRF_TRUSTED_ORIGINS = ['https://moo.dev.shacklyn.net', 'https://probe.cluster.l
 DATABASES = {
     'default': {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "moo",
-        "HOST": "moo-db.dev.shacklyn.net",
-        "USER": "moo",
-        "PASSWORD": "moo"
+        "NAME": "termiverse",
+        "HOST": "termiverse-db.dev.shacklyn.net",
+        "USER": "termiverse",
+        "PASSWORD": "termiverse"
     }
 }
 
