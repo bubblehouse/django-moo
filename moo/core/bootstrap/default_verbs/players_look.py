a1e56934fe7ab450bf9578d7ce18350592e3d766
Phil Christensen <phil@bubblehouse.org>
2024-05-06 12:08:42 -0400

    fix: improved `look` command with better functionality and ANSI colors

--- moo/core/bootstrap/default_verbs/players_look.py
+++ moo/core/bootstrap/default_verbs/players_look.py
@@ -1,7 +1,27 @@
 from moo.core import api
 
-qs = api.caller.location.properties.filter(name="description")
-if qs:
-    print(qs[0].value)
+if(api.parser.has_dobj()):
+    obj = api.parser.get_dobj()
+elif(api.parser.has_dobj_str()):
+    dobj_str = api.parser.get_dobj_str()
+    qs = api.caller.find(dobj_str)
+    if not qs:
+        print(f"There is no '{dobj_str}' here.")
+        return  # pylint: disable=return-outside-function  # type: ignore
+    obj = qs[0]
 else:
-    print("No description.")
+    obj = api.caller.location
+
+qs = obj.properties.filter(name="description")
+if qs:
+    print(f"""
+[yellow]{obj.name}[/yellow]
+
+[deep_sky_blue1]{qs[0].value}[/deep_sky_blue1]
+""")
+else:
+    print(f"""
+[yellow]{obj.name}[/yellow]
+
+[deep_pink4 bold]Not much to see here.[/deep_pink4 bold]
+""")
