59baa003d583d3ddd62602204810327065700b7d
Phil Christensen <phil@bubblehouse.org>
2024-06-28 12:04:13 -0400

    fix: consolidate custom verb functions in moo.core

--- moo/core/code.py
+++ moo/core/code.py
@@ -12,7 +12,6 @@ from django.conf import settings
 from RestrictedPython import compile_restricted, compile_restricted_function
 from RestrictedPython.Guards import safe_builtins
 from RestrictedPython.Guards import guarded_unpack_sequence
-from kombu import Exchange, Queue
 
 log = logging.getLogger(__name__)
 
@@ -26,26 +25,6 @@ def interpret(source, *args, runtype="exec", **kwargs):
         return r_exec(source, {}, globals)
     else:
         return r_eval(source, {}, globals)
-
-def message_user(user_obj, message, producer=None):
-    from .models.auth import Player
-    try:
-        player = Player.objects.get(avatar=user_obj)
-    except Player.DoesNotExist:
-        return
-    from ..celery import app
-    with app.default_connection() as conn:
-        channel = conn.channel()
-        queue = Queue('messages', Exchange('moo', type='direct', channel=channel), f'user-{player.user.pk}', channel=channel)
-        with app.producer_or_acquire(producer) as producer:  #  pylint: disable=redefined-argument-from-local
-            producer.publish(
-                dict(message=message, caller=context.get('caller')),
-                serializer='pickle',
-                exchange=queue.exchange,
-                routing_key=f'user-{player.user.pk}',
-                declare=[queue],
-                retry=True,
-            )
 
 def compile_verb_code(body, filename):
     """
@@ -143,7 +122,7 @@ def get_restricted_environment(writer):
 
     for name in settings.ALLOWED_BUILTINS:
         safe_builtins[name] = __builtins__[name]
-    from .models.object import create_object
+    from moo.core import create_object
     env = dict(
         _apply_           = lambda f,*a,**kw: f(*a, **kw),
         _print_           = lambda x: _print_(),
@@ -157,7 +136,6 @@ def get_restricted_environment(writer):
         __import__        = restricted_import,
         __builtins__      = safe_builtins,
         create_object     = create_object,
-        message_user      = message_user
     )
 
     return env
