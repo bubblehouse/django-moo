2a4d34f490915f486e95547da5edfb44adfaecdc
Phil Christensen <phil@bubblehouse.org>
2024-07-04 14:41:44 -0400

    docs: more docstrings

--- docs/source/guide/10_tasks.md
+++ docs/source/guide/10_tasks.md
@@ -1,3 +1,32 @@
+## MOO Tasks
+
+Each task in the DjangoMOO environment is executed asynchronously by the Celery workers.
+
+### Available Tasks
+```{eval-rst}
+.. py:currentmodule:: moo.core.tasks
+.. autofunction:: parse_command
+.. autofunction:: parse_code
+```
+
+### Task Security
+
+The running of a task is kept reasonably secure and isolated in a few ways:
+
+* Each Verb execution runs in a Celery worker; if a memory race is triggered,
+  it's limited to the worker process, which **CAN** be limited by Unix process management.
+* Before Verb execution, a new atomic DB transaction is started. This **SHOULD** prevent
+  conflicts when two Verbs are trying to modify the same object.
+* Verb runtime itself is limited to 3 seconds (configurable). This **SHOULD** force the Verb
+  to complete in order to commit the changes to the database.
+
 ### Executing Statements at a Later Time
 
-## MOO Tasks
+But if you're limited to 3 seconds in a Verb, how do you create longer tasks? The key is to
+compose your function into multiple Verb calls, which you can invoke asynchronously using the
+Task class:
+
+```{eval-rst}
+.. py:currentmodule:: moo.core.models
+.. autoclass:: Task
+```
--- moo/core/tasks.py
+++ moo/core/tasks.py
@@ -4,6 +4,7 @@ Celery Tasks for executing commands or raw Python code.
 """
 
 import logging
+from typing import Any
 
 from django.db import transaction
 from celery import shared_task
@@ -14,9 +15,14 @@ from .models import Object
 log = logging.getLogger(__name__)
 
 @shared_task
-def parse_command(caller_id, line):
+def parse_command(caller_id:int, line:str) -> list[Any]:
     """
-    Execute a textual command in a task.
+    Parse a command-line and invoke the requested verb.
+
+    :param caller_id: the PK of the caller of this command
+    :param line: the natural-language command to parse and execute
+    :return: a list of output lines
+    :raises UserError: if a verb failure happens
     """
     output = []
     with transaction.atomic():
@@ -31,9 +37,13 @@ def parse_command(caller_id, line):
     return output
 
 @shared_task
-def parse_code(caller_id, source, runtype="exec"):
+def parse_code(caller_id:Object, source:str, runtype:str="exec") -> list[list[Any], Any]:
     """
-    Execute prompt code in a task.
+    Execute code in a task.
+
+    :param caller_id: the PK of the caller of this command
+    :param source: the Python code to execute
+    :return: a list of output lines and the result value, if any
     """
     output = []
     with transaction.atomic():
