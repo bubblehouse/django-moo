16bbe5fa4000f7f9d94b70dfe25b428f94dde3f8
Phil Christensen <phil@bubblehouse.org>
2024-07-07 15:21:06 -0400

    test: added simple async verb test

--- moo/settings/test.py
+++ moo/settings/test.py
@@ -3,3 +3,6 @@ from .base import *  # pylint: disable=wildcard-import
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-061+p62f39ohlfrgu&)%1lxo%%#_-$rc5l_zsrlx6jqy)sw(=r"
+
+CELERY_TASK_ALWAYS_EAGER = True
+CELERY_TASK_STORE_EAGER_RESULT = True
