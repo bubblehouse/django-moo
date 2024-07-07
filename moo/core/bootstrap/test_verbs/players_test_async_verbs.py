7fc0ee54ec9765ae7e40e5eea03f6cc83a83a75f
Phil Christensen <phil@bubblehouse.org>
2024-07-07 15:21:06 -0400

    test: added simple async verb test

---
+++ moo/core/bootstrap/test_verbs/players_test_async_verbs.py
@@ -0,0 +1,11 @@
+from moo.core import api, invoke
+
+counter = 1
+if args and len(args):  # pylint: disable=undefined-variable
+    counter = args[0] + 1  # pylint: disable=undefined-variable
+
+print(counter)
+
+if counter < 10:
+    verb = api.caller.get_verb('test-async-verbs')
+    invoke(counter, verb=verb)
