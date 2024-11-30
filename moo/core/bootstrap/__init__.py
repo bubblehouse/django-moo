ba7428439d2a1bd4f293817e066cacc9236f0781
Phil Christensen <phil.christensen@eaze.com>
2024-11-29 22:08:34 -0500

    refactor: minor revisions

--- moo/core/bootstrap/__init__.py
+++ moo/core/bootstrap/__init__.py
@@ -8,14 +8,14 @@ import shlex
 import importlib.resources
 import logging
 
+log = logging.getLogger(__name__)
+
 parser = argparse.ArgumentParser('moo')
 parser.add_argument('subcommand', choices=['verb'])
 parser.add_argument('names', nargs='+')
 parser.add_argument('--on', help='The object to add or modify the verb on')
 parser.add_argument('--ability', action='store_true', help='Whether the verb is an intrinsic ability')
 parser.add_argument('--method', action='store_true', help='Whether the verb is a method (callable from verb code)')
-
-log = logging.getLogger(__name__)
 
 def get_source(filename, dataset='default'):
     ref = importlib.resources.files('moo.core.bootstrap') / f'{dataset}_verbs/{filename}'
@@ -36,14 +36,13 @@ def load_python(python_path):
 def load_verbs(repo, dataset='default'):
     from moo.core.models.object import Object
     for ref in importlib.resources.files(f'moo.core.bootstrap.{dataset}_verbs').iterdir():
-        if ref.name.startswith('_') or not ref.name.endswith('.py'):
-            continue
-        log.info(f"Loading verb source `{ref.name}`...")
         with ref.open() as f:
             contents = f.read()
             first, code = contents.split("\n", maxsplit=1)
             if first.startswith("#!moo "):
+                log.info(f"Loading verb source `{ref.name}`...")
                 args = parser.parse_args(shlex.split(first[6:]))
                 obj = Object.objects.get(name=args.on)
                 obj.add_verb(*args.names, code=code, filename=ref.name, repo=repo, ability=args.ability, method=args.method)
-                
+            else:
+                log.info(f"Skipping verb source `{ref.name}`...")
