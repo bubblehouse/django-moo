3312f1d048f38ea429f91467fcc8ca123ef49f7c
Phil Christensen <phil.christensen@eaze.com>
2024-12-01 19:37:03 -0500

    fix: restore default bootstrap after mistaking it for test

--- moo/core/bootstrap/default.py
+++ moo/core/bootstrap/default.py
@@ -12,17 +12,32 @@ containers = lookup('containers class')
 with code.context(wizard, log.info):
     bag = create('bag of holding', parents=[containers], location=wizard)
     hammer = create('wizard hammer', location=bag)
-    players = create('player class', location=bag)
+    book = create('class book', parents=[containers], location=bag)
+    players = create('player class', location=book)
+    guests = create('guest class', location=book)
+    guests.parents.add(players)
+    authors = create('author class', location=book)
+    authors.parents.add(players)
+    programmers = create('programmer class', location=book)
+    programmers.parents.add(authors)
+    wizards = create('wizard class', location=book)
+    wizards.parents.add(programmers)
 
-    rooms = create('room class', parents=[containers], location=bag)
+    wizard.parents.add(wizards)
+
+    rooms = create('room class', parents=[containers], location=book)
     rooms.set_property("description", "There's not much to see here.", inherited=True)
 
-    lab = create('The Laboratory', parents=[rooms])
+    lab = create('The Laboratory')
+    lab.parents.add(rooms)
     lab.set_property("description", """A cavernous laboratory filled with gadgetry of every kind,
     this seems like a dumping ground for every piece of dusty forgotten
     equipment a mad scientist might require.""")
 
     wizard.location = lab
     wizard.save()
+
+    player = create(name="Player", unique_name=True, location=lab)
+    player.parents.add(players, containers)
 
     bootstrap.load_verbs(repo, dataset='default')
