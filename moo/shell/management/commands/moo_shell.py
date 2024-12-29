c299e3ae593c2c8c353c8702d1ed9d0bd60854a9
Phil Christensen <phil@bubblehouse.org>
2024-12-29 17:44:31 -0500

    fix: logging improvements for shell server

--- moo/shell/management/commands/moo_shell.py
+++ moo/shell/management/commands/moo_shell.py
@@ -1,16 +1,14 @@
 # -*- coding: utf-8 -*-
-import logging
 import asyncio
 
 from django.core.management.base import BaseCommand
 
 from ...server import server
 
-log = logging.getLogger(__name__)
-
 class Command(BaseCommand):
     help = 'Run the moo SSH server.'
 
     def handle(self, *args, **options):
-        log.info("Starting shell server...")
+        import logging
+        logging.info("Starting shell server...")
         asyncio.run(server())
