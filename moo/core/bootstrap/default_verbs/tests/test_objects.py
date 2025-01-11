25ac2178b2eb8486d9e2f7d7e5e31e78d8432bdd
Phil Christensen <phil@bubblehouse.org>
2025-01-11 15:27:24 -0500

    test: added test for make

--- moo/core/bootstrap/default_verbs/tests/test_objects.py
+++ moo/core/bootstrap/default_verbs/tests/test_objects.py
@@ -12,13 +12,13 @@ def test_creation_and_description(t_init: Object, t_wizard: Object):
     with code.context(t_wizard, _writer):
         parse.interpret("make a widget")
         assert printed == [
-            '[color yellow]Created #15 (a widget)[/color yellow]',
+            '[color yellow]Created #15 (widget)[/color yellow]',
         ]
         printed.clear()
 
         parse.interpret("make a jar from container")
         assert printed == [
-            '[color yellow]Created #16 (a jar)[/color yellow]',
-            '[color yellow]Transmuted #16 (a jar) to #2 (container class)[/color yellow]'
+            '[color yellow]Created #16 (jar)[/color yellow]',
+            '[color yellow]Transmuted #16 (jar) to #2 (container class)[/color yellow]'
         ]
         printed.clear()
