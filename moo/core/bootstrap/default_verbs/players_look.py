b2f15242498afedcaa253461de3dedddb4f568b4
Phil Christensen <phil@bubblehouse.org>
2025-01-12 13:32:33 -0500

    fix: allow more look scenarios, update test

--- moo/core/bootstrap/default_verbs/players_look.py
+++ moo/core/bootstrap/default_verbs/players_look.py
@@ -4,20 +4,28 @@ from moo.core import api
 
 if(api.parser.has_dobj()):
     obj = api.parser.get_dobj()
+
 elif(api.parser.has_dobj_str()):
     dobj_str = api.parser.get_dobj_str()
-    qs = api.caller.find(dobj_str)
+    qs = api.caller.find(dobj_str) or api.caller.location.find(dobj_str)
     if not qs:
         print(f"There is no '{dobj_str}' here.")
         return  # pylint: disable=return-outside-function  # type: ignore
     obj = qs[0]
+elif(api.parser.has_pobj_str('at')):
+    pobj_str = api.parser.get_pobj_str('at')
+    qs = api.caller.find(pobj_str) or api.caller.location.find(pobj_str)
+    if not qs:
+        print(f"There is no '{pobj_str}' here.")
+        return  # pylint: disable=return-outside-function  # type: ignore
+    obj = qs[0]
+
 else:
     obj = api.caller.location
 
 print(f"[bright_yellow]{obj.name}[/bright_yellow]")
-has_description = obj.properties.filter(name="description")
-if has_description:
-    print(f"[deep_sky_blue1]{has_description[0].value}[/deep_sky_blue1]")
+if obj.has_property('description'):
+    print(f"[deep_sky_blue1]{obj.get_property('description')}[/deep_sky_blue1]")
 else:
     print("[deep_pink4 bold]Not much to see here.[/deep_pink4 bold]")
 
