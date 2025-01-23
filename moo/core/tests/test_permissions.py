5671f8e4c00f0ef7ef6543553c15d8169c13da78
Phil Christensen <phil@bubblehouse.org>
2025-01-23 16:52:48 -0500

    style: reformat with isort

--- moo/core/tests/test_permissions.py
+++ moo/core/tests/test_permissions.py
@@ -4,99 +4,114 @@ import pytest
 from django.test import override_settings
 
 from moo.core.models import Object, Player
-from .. import code, exceptions, create, lookup
+
+from .. import code, create, exceptions, lookup
+
 
 @pytest.mark.django_db
 def test_regular_user_can_read_a_thing(t_init: Object, t_wizard: Object):
-    thing = Object.objects.create(name='thing', owner=t_wizard)
-    user = Object.objects.get(name__iexact='player')
-    assert user.is_allowed('read', thing)
-    assert not user.is_allowed('write', thing)
-    assert not user.is_allowed('entrust', thing)
-    assert not user.is_allowed('move', thing)
-    assert not user.is_allowed('transmute', thing)
-    assert not user.is_allowed('derive', thing)
-    assert not user.is_allowed('develop', thing)
+    thing = Object.objects.create(name="thing", owner=t_wizard)
+    user = Object.objects.get(name__iexact="player")
+    assert user.is_allowed("read", thing)
+    assert not user.is_allowed("write", thing)
+    assert not user.is_allowed("entrust", thing)
+    assert not user.is_allowed("move", thing)
+    assert not user.is_allowed("transmute", thing)
+    assert not user.is_allowed("derive", thing)
+    assert not user.is_allowed("develop", thing)
+
 
 @pytest.mark.django_db
 def test_regular_user_who_owns_a_thing(t_init: Object, t_wizard: Object):
-    user = Object.objects.get(name__iexact='player')
-    thing = Object.objects.create(name='thing', owner=user)
-    assert user.is_allowed('read', thing)
-    assert user.is_allowed('write', thing)
-    assert user.is_allowed('entrust', thing)
-    assert user.is_allowed('move', thing)
-    assert user.is_allowed('transmute', thing)
-    assert user.is_allowed('derive', thing)
-    assert user.is_allowed('develop', thing)
+    user = Object.objects.get(name__iexact="player")
+    thing = Object.objects.create(name="thing", owner=user)
+    assert user.is_allowed("read", thing)
+    assert user.is_allowed("write", thing)
+    assert user.is_allowed("entrust", thing)
+    assert user.is_allowed("move", thing)
+    assert user.is_allowed("transmute", thing)
+    assert user.is_allowed("derive", thing)
+    assert user.is_allowed("develop", thing)
+
 
 @pytest.mark.django_db
 def test_everyone_can_read_a_thing(t_init: Object, t_wizard: Object):
-    thing = Object.objects.create(name='thing')
-    jim = Object.objects.create(name='Jim', unique_name=True)
-    assert jim.is_allowed('read', thing)
-    assert not jim.is_allowed('write', thing)
-    assert not jim.is_allowed('entrust', thing)
-    assert not jim.is_allowed('move', thing)
-    assert not jim.is_allowed('transmute', thing)
-    assert not jim.is_allowed('derive', thing)
-    assert not jim.is_allowed('develop', thing)
+    thing = Object.objects.create(name="thing")
+    jim = Object.objects.create(name="Jim", unique_name=True)
+    assert jim.is_allowed("read", thing)
+    assert not jim.is_allowed("write", thing)
+    assert not jim.is_allowed("entrust", thing)
+    assert not jim.is_allowed("move", thing)
+    assert not jim.is_allowed("transmute", thing)
+    assert not jim.is_allowed("derive", thing)
+    assert not jim.is_allowed("develop", thing)
+
 
 @pytest.mark.django_db
 def test_wizard_can_do_most_things(t_init: Object, t_wizard: Object):
     # Objects are only Wizards if they have an associated Player with wizard=True
     Player.objects.create(avatar=t_wizard, wizard=True)
-    thing = Object.objects.create(name='thing')
-    assert t_wizard.is_allowed('read', thing)
-    assert t_wizard.is_allowed('write', thing)
-    assert t_wizard.is_allowed('entrust', thing)
-    assert t_wizard.is_allowed('move', thing)
-    assert t_wizard.is_allowed('transmute', thing)
-    assert t_wizard.is_allowed('derive', thing)
-    assert t_wizard.is_allowed('develop', thing)
+    thing = Object.objects.create(name="thing")
+    assert t_wizard.is_allowed("read", thing)
+    assert t_wizard.is_allowed("write", thing)
+    assert t_wizard.is_allowed("entrust", thing)
+    assert t_wizard.is_allowed("move", thing)
+    assert t_wizard.is_allowed("transmute", thing)
+    assert t_wizard.is_allowed("derive", thing)
+    assert t_wizard.is_allowed("develop", thing)
+
 
 @pytest.mark.django_db
 def test_add_a_simple_deny_clase(t_init: Object, t_wizard: Object):
-    user = Object.objects.get(name__iexact='player')
-    thing = Object.objects.create(name='thing', owner=user)
-    thing.allow('everyone', 'anything')
-    thing.deny(user, 'write')
+    user = Object.objects.get(name__iexact="player")
+    thing = Object.objects.create(name="thing", owner=user)
+    thing.allow("everyone", "anything")
+    thing.deny(user, "write")
 
-    assert user.is_allowed('read', thing)
-    assert not user.is_allowed('write', thing)
+    assert user.is_allowed("read", thing)
+    assert not user.is_allowed("write", thing)
+
 
 @pytest.mark.django_db
 def test_cant_create_child_of_an_object_that_isnt_yours(t_init: Object, t_wizard: Object):
     printed = []
+
     def _writer(msg):
         printed.append(msg)
-    user = Object.objects.get(name__iexact='player')
-    parent_thing = Object.objects.create(name='parent thing', owner=t_wizard)
+
+    user = Object.objects.get(name__iexact="player")
+    parent_thing = Object.objects.create(name="parent thing", owner=t_wizard)
     with code.context(user, _writer):
-        child_thing = Object.objects.create(name='child thing', owner=user)
+        child_thing = Object.objects.create(name="child thing", owner=user)
         with pytest.raises(PermissionError) as excinfo:
             child_thing.parents.add(parent_thing)
         assert str(excinfo.value) == f"#{user.pk} (Player) is not allowed derive on #{parent_thing.pk} (parent thing)"
 
+
 @pytest.mark.django_db
 def test_cant_create_parent_of_an_object_that_isnt_yours(t_init: Object, t_wizard: Object):
     printed = []
+
     def _writer(msg):
         printed.append(msg)
-    user = Object.objects.get(name__iexact='player')
-    child_thing = Object.objects.create(name='child thing', owner=t_wizard)
+
+    user = Object.objects.get(name__iexact="player")
+    child_thing = Object.objects.create(name="child thing", owner=t_wizard)
     with code.context(user, _writer):
-        parent_thing = Object.objects.create(name='parent thing', owner=user)
+        parent_thing = Object.objects.create(name="parent thing", owner=user)
         with pytest.raises(PermissionError) as excinfo:
             child_thing.parents.add(parent_thing)
         assert str(excinfo.value) == f"#{user.pk} (Player) is not allowed transmute on #{child_thing.pk} (child thing)"
 
+
 @pytest.mark.django_db
 def test_cant_change_owner_unless_allowed_to_entrust(t_init: Object, t_wizard: Object):
     printed = []
+
     def _writer(msg):
         printed.append(msg)
-    user = Object.objects.get(name__iexact='player')
+
+    user = Object.objects.get(name__iexact="player")
     with code.context(user, _writer):
         with pytest.raises(PermissionError) as excinfo:
             create("thing", owner=t_wizard)
@@ -108,21 +123,24 @@ def test_cant_change_owner_unless_allowed_to_entrust(t_init: Object, t_wizard: O
         assert str(excinfo.value) == f"#{user.pk} (Player) is not allowed entrust on #{obj.pk} (thing)"
     with code.context(t_wizard, _writer):
         obj = lookup("thing")
-        obj.allow(user, 'entrust')
-        obj.allow(user, 'write')
+        obj.allow(user, "entrust")
+        obj.allow(user, "write")
     with code.context(user, _writer):
         obj = lookup("thing")
         obj.owner = t_wizard
         obj.save()
 
+
 @pytest.mark.django_db
 def test_cant_change_location_unless_allowed_to_move(t_init: Object, t_wizard: Object):
     printed = []
+
     def _writer(msg):
         printed.append(msg)
+
     with code.context(t_wizard, _writer):
         obj = create("thing")
-    user = Object.objects.get(name__iexact='player')
+    user = Object.objects.get(name__iexact="player")
     with code.context(user, _writer):
         obj = lookup("thing")
         obj.location = user
@@ -131,8 +149,8 @@ def test_cant_change_location_unless_allowed_to_move(t_init: Object, t_wizard: O
         assert str(excinfo.value) == f"#{user.pk} (Player) is not allowed write on #{obj.pk} (thing)"
     with code.context(t_wizard, _writer):
         obj = lookup("thing")
-        obj.allow(user, 'move')
-        obj.allow(user, 'write')
+        obj.allow(user, "move")
+        obj.allow(user, "write")
     with code.context(user, _writer):
         obj = lookup("thing")
         obj.location = user
@@ -143,8 +161,10 @@ def test_cant_change_location_unless_allowed_to_move(t_init: Object, t_wizard: O
 @override_settings(CELERY_TASK_ALWAYS_EAGER=True, CELERY_TASK_STORE_EAGER_RESULT=True)
 def test_change_location_calls_enterfunc(t_init: Object, t_wizard: Object, caplog: pytest.LogCaptureFixture):
     printed = []
+
     def _writer(msg):
         printed.append(msg)
+
     with caplog.at_level(logging.INFO, "moo.core.tasks.background"):
         with code.context(t_wizard, _writer):
             containers = lookup("container class")
@@ -155,12 +175,15 @@ def test_change_location_calls_enterfunc(t_init: Object, t_wizard: Object, caplo
             thing.save()
     assert f"#{thing.pk} (thing)\n" in caplog.text
 
+
 @pytest.mark.django_db
 @override_settings(CELERY_TASK_ALWAYS_EAGER=True, CELERY_TASK_STORE_EAGER_RESULT=True)
 def test_change_location_calls_exitfunc(t_init: Object, t_wizard: Object, caplog: pytest.LogCaptureFixture):
     printed = []
+
     def _writer(msg):
         printed.append(msg)
+
     with caplog.at_level(logging.INFO, "moo.core.tasks.background"):
         with code.context(t_wizard, _writer):
             containers = lookup("container class")
@@ -175,12 +198,15 @@ def test_change_location_calls_exitfunc(t_init: Object, t_wizard: Object, caplog
             assert thing.location == t_wizard.location
     assert f"#{thing.pk} (thing)\n" in caplog.text
 
+
 @pytest.mark.django_db
 def test_change_location_calls_accept(t_init: Object, t_wizard: Object):
     printed = []
+
     def _writer(msg):
         printed.append(msg)
-    user = Object.objects.get(name__iexact='player')
+
+    user = Object.objects.get(name__iexact="player")
     with code.context(user, _writer):
         box = create("box")
         box.add_verb("accept", code="return False", method=True)
@@ -189,11 +215,14 @@ def test_change_location_calls_accept(t_init: Object, t_wizard: Object):
         thing = lookup("thing")
         assert str(excinfo.value) == f"#{box.pk} (box) did not accept #{thing.pk} (thing)"
 
+
 @pytest.mark.django_db
 def test_change_location_checks_recursion(t_init: Object, t_wizard: Object):
     printed = []
+
     def _writer(msg):
         printed.append(msg)
+
     with code.context(t_wizard, _writer):
         containers = lookup("container class")
         box = create("box", parents=[containers])
--- moo/core/utils.py
+++ moo/core/utils.py
@@ -5,12 +5,13 @@ Useful global utilities.
 
 import warnings
 
+
 def apply_default_permissions(instance):
     from .models import Object
     from .models.verb import AccessibleVerb
+
     set_default_permissions = AccessibleVerb.objects.filter(
-        origin = Object.objects.get(pk=1),
-        names__name = 'set_default_permissions'
+        origin=Object.objects.get(pk=1), names__name="set_default_permissions"
     )
     if set_default_permissions:
         set_default_permissions[0](instance)
