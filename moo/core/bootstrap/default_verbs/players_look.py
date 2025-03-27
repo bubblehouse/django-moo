a8568bdca86d716bcfa0d8d95f9cc677c147f2d9
Phil Christensen <phil@bubblehouse.org>
2025-03-27 12:42:55 -0400

    fix: improve method handling to handle system.describe() implementation

--- moo/core/bootstrap/default_verbs/players_look.py
+++ moo/core/bootstrap/default_verbs/players_look.py
@@ -1,10 +1,11 @@
 #!moo verb look inspect --on "player class" --ability
 
-from moo.core import api
+from moo.core import api, lookup
+
+system = lookup("system object")
 
 if api.parser.has_dobj():
     obj = api.parser.get_dobj()
-
 elif api.parser.has_dobj_str():
     dobj_str = api.parser.get_dobj_str()
     qs = api.caller.find(dobj_str) or api.caller.location.find(dobj_str)
@@ -19,26 +20,7 @@ elif api.parser.has_pobj_str("at"):
         print(f"There is no '{pobj_str}' here.")
         return  # pylint: disable=return-outside-function  # type: ignore
     obj = qs[0]
-
 else:
     obj = api.caller.location
 
-print(f"[bright_yellow]{obj.name}[/bright_yellow]")
-if obj.has_property("description"):
-    print(f"[deep_sky_blue1]{obj.get_property('description')}[/deep_sky_blue1]")
-else:
-    print("[deep_pink4 bold]Not much to see here.[/deep_pink4 bold]")
-
-contents = obj.contents.filter(obvious=True)
-if contents:
-    print("[yellow]Obvious contents:[/yellow]")
-    for content in contents:
-        print(f"{content.name}")
-
-# if you're looking at the room you're in, show the exits
-if obj == api.caller.location and obj.has_property("exits"):
-    exits = api.caller.location.get_property("exits")
-    if exits:
-        print("[yellow]Exits:[/yellow]")
-        for direction, _ in exits.items():
-            print(direction)
+print(system.describe(obj))
