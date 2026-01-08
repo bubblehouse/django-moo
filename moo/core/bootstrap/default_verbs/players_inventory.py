f099457dedc22e40bc5862091ad9a4b93d66b13c
Phil Christensen <phil.christensen@eaze.com>
2026-01-07 19:06:13 -0500

    fix: update inventory verb

--- moo/core/bootstrap/default_verbs/players_inventory.py
+++ moo/core/bootstrap/default_verbs/players_inventory.py
@@ -4,9 +4,9 @@
 This verb is used to tell a player what s/he has in his/her pockets.
 """
 
-if this.contents:
+if this.contents.count() > 0:
   this.tell("You are carrying:")
-  for thing in this.contents:
+  for thing in this.contents.all():
     this.tell(thing.title())
 else:
   this.tell("You are empty-handed.")
