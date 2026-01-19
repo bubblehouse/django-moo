c24da56d8061a015f22861aa9c38fed1d4777049
Phil Christensen <phil.christensen@eaze.com>
2026-01-19 11:34:02 -0500

    chore: seeding more room verbs

---
+++ moo/core/bootstrap/default_verbs/exit/recycle.py
@@ -0,0 +1,8 @@
+#!moo verb recycle --on $exit
+
+"""
+This verb is defined to allow an exit to be tidily removed from the database. The exit is removed from the entrance and
+exit lists of the destination and source rooms respectively, if the caller of this verb has permission to do so. This
+is done using the `$room.remove_(entrance|exit)` verbs.
+"""
+
