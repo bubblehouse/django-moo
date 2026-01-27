769dda2158a13fbe1ec7101eb185b4412b2caab6
Phil Christensen <phil.christensen@eaze.com>
2026-01-27 14:17:17 -0500

    test: switch between creator and user objects to test permissions

--- moo/core/bootstrap/default_verbs/_system_set_default_permissions.py
+++ moo/core/bootstrap/default_verbs/_system_set_default_permissions.py
@@ -1,6 +1,7 @@
 from moo.core import api
 
 obj = args[0]  # pylint: disable=undefined-variable
+
 obj.allow("wizards", "anything")
 obj.allow("owners", "anything")
 obj.allow("everyone", "read")
--- moo/core/bootstrap/default_verbs/tests/test_rooms.py
+++ moo/core/bootstrap/default_verbs/tests/test_rooms.py
@@ -1,7 +1,7 @@
 import pytest
 
-from moo.core import code, parse
-from moo.core.models import Object
+from moo.core import code, parse, lookup
+from moo.core.models import Object, Player
 
 
 @pytest.mark.django_db(transaction=True, reset_sequences=True)
@@ -12,6 +12,7 @@ def test_basic_dig_and_tunnel(t_init: Object, t_wizard: Object):
     def _writer(msg):
         printed.append(msg)
 
+    avatar = lookup("Player")
     with code.context(t_wizard, _writer) as ctx:
         home_location = t_wizard.location
         parse.interpret(ctx, "@dig north to Another Room")
@@ -24,28 +25,33 @@ def test_basic_dig_and_tunnel(t_init: Object, t_wizard: Object):
         assert printed == ["[color red]There is already an exit in that direction.[/color red]"]
         printed.clear()
 
+    with code.context(avatar, _writer) as ctx:
         with pytest.warns(RuntimeWarning, match=r"ConnectionError") as warnings:
             parse.interpret(ctx, "go north")
         assert [str(x.message) for x in warnings.list] == [
-            "ConnectionError(#3 (Wizard)): You leave #14 (The Laboratory).",
-            "ConnectionError(#3 (Wizard)): You arrive at #17 (Another Room)."
+            "ConnectionError(#15 (Player)): You leave #14 (The Laboratory).",
+            "ConnectionError(#3 (Wizard)): #15 (Player) leaves #14 (The Laboratory).",
+            "ConnectionError(#15 (Player)): You arrive at #17 (Another Room)."
         ]
-        t_wizard.refresh_from_db()
-        assert t_wizard.location.name == "Another Room"
+        avatar.refresh_from_db()
+        assert avatar.location.name == "Another Room"
         printed.clear()
 
+    with code.context(t_wizard, _writer) as ctx:
         parse.interpret(ctx, f"@tunnel south to {home_location.name}")
         assert printed == [
             f'[color yellow]Tunnelled an exit south to "{home_location.name}".[/color yellow]',
         ]
         printed.clear()
 
+    with code.context(avatar, _writer) as ctx:
         with pytest.warns(RuntimeWarning, match=r"ConnectionError") as warnings:
             parse.interpret(ctx, "go south")
         assert [str(x.message) for x in warnings.list] == [
-            "ConnectionError(#3 (Wizard)): You leave #17 (Another Room).",
-            "ConnectionError(#3 (Wizard)): You arrive at #14 (The Laboratory)."
+            "ConnectionError(#15 (Player)): You leave #17 (Another Room).",
+            "ConnectionError(#15 (Player)): You arrive at #14 (The Laboratory).",
+            "ConnectionError(#3 (Wizard)): #15 (Player) arrives in #14 (The Laboratory)."
         ]
-        t_wizard.refresh_from_db()
-        assert t_wizard.location.name == home_location.name
+        avatar.refresh_from_db()
+        assert avatar.location.name == home_location.name
         printed.clear()
