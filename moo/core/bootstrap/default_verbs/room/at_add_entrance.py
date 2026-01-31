7d7a58deec90a589a91d5b6e37d426890d498655
Phil Christensen <phil@bubblehouse.org>
2026-01-31 17:42:55 -0500

    feat: implemented support verbs for exits

--- moo/core/bootstrap/default_verbs/room/at_add_entrance.py
+++ moo/core/bootstrap/default_verbs/room/at_add_entrance.py
@@ -10,3 +10,13 @@ is aborted with a suitable error message.
 Otherwise, if the object found is a descendant of the `$exit` class, then the exit is checked to make sure it goes to
 this room. If this is the case, then the exit is added as an entrance using the room's `add_entrance` verb.
 """
+
+from moo.core import api
+
+door = api.parser.get_dobj()
+if not door.is_a(_.exit):
+    print("[color red]The specified object is not an exit.[/color red]")
+    return
+
+this.add_entrance(door)
+print(f'[color yellow]Added entrance "{door.name}".[/color yellow]')
