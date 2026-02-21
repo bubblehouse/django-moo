00fa31e4b2da55b7fe8c45e3b1b751f7ecc71de9
Phil Christensen <phil.christensen@eaze.com>
2026-02-21 13:44:58 -0500

    fix: --on is required in the moo shebang line

--- moo/core/bootstrap/__init__.py
+++ moo/core/bootstrap/__init__.py
@@ -35,7 +35,7 @@ class ISpecAction(argparse.Action):
 parser = argparse.ArgumentParser("moo")
 parser.add_argument("subcommand", choices=["verb"])
 parser.add_argument("names", nargs="+")
-parser.add_argument("--on", help="The object to add or modify the verb on")
+parser.add_argument("--on", help="The object to add or modify the verb on", required=True)
 parser.add_argument("--dspec", choices=["this", "any", "none", "either"], default="none", help="The direct object specifier")
 parser.add_argument("--ispec", metavar="PREP:SPEC", nargs="+", help="Indirect object specifiers", action=ISpecAction)
 
