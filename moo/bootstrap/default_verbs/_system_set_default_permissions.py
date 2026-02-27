6e273355faf453c8bdeaf096079755be33ddb387
Phil Christensen <phil.christensen@eaze.com>
2026-02-27 17:32:11 -0500

    fix: fixed remaining caller_stack issues

--- moo/bootstrap/default_verbs/_system_set_default_permissions.py
+++ moo/bootstrap/default_verbs/_system_set_default_permissions.py
@@ -1,14 +1,15 @@
 # pylint: disable=undefined-variable
 
+from moo.core import context, set_task_perms
+
 obj = args[0]
+with set_task_perms(context.player):
+    obj.allow("wizards", "anything")
+    obj.allow("owners", "anything")
 
-obj.allow("wizards", "anything")
-obj.allow("owners", "anything")
-obj.allow("everyone", "read")
-
-if obj.kind == "verb":
-    obj.allow("everyone", "execute")
-elif obj.kind == "property":
-    obj.allow("everyone", "read")
-elif obj.kind == "object":
-    obj.allow("everyone", "read")
+    if obj.kind == "verb":
+        obj.allow("everyone", "execute")
+    elif obj.kind == "property":
+        obj.allow("everyone", "read")
+    elif obj.kind == "object":
+        obj.allow("everyone", "read")
