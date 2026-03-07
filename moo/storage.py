5f31fc4a208cc50405393c525fd805728832da85
Phil Christensen <phil.christensen@eaze.com>
2026-03-07 12:21:55 -0500

    feat: use S3 for static assets

---
+++ moo/storage.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from django.core.files.storage import get_storage_class
+from storages.backends.s3boto3 import S3Boto3Storage
+
+class CachedS3Boto3Storage(S3Boto3Storage):
+    """
+    S3 storage backend that saves the files locally, too.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.local_storage = get_storage_class(
+            "compressor.storage.CompressorFileStorage")()
+
+    def save(self, name, content):
+        self.local_storage._save(name, content)  # pylint: disable=protected-access
+        super().save(name, self.local_storage._open(name))  # pylint: disable=protected-access
+        return name
