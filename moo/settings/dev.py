b54c6f6dfb39f3870639595205d1ae70fd60e66d
Phil Christensen <phil@bubblehouse.org>
2026-03-07 13:41:39 -0500

    fix: added missing setting

--- moo/settings/dev.py
+++ moo/settings/dev.py
@@ -20,6 +20,8 @@ STORAGES = {
         "BACKEND": 'moo.storage.CachedS3Boto3Storage',
     },
 }
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'moo.storage.CachedS3Boto3Storage'
 AWS_STORAGE_BUCKET_NAME = 'django-moo-dev-assets-386413725601-us-east-2'
 AWS_S3_ADDRESSING_STYLE = "virtual"
 
