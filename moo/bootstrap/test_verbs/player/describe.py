22da21e418b6b5478262dd78b7e184e83d19fdab
Phil Christensen <phil@bubblehouse.org>
2026-03-11 14:40:05 -0400

    test: remove default verbs from test dataset

--- moo/bootstrap/test_verbs/player/describe.py
+++
@@ -1,5 +0,0 @@
-#!moo verb desc*ribe --on "player class" --dspec any
-
-# pylint: disable=return-outside-function,undefined-variable
-
-print("Would describe an object here.")
--- moo/bootstrap/test_verbs/player/test_async_verb_callback.py
+++
@@ -1,5 +0,0 @@
-#!moo verb test-async-verb-callback --on "player class" --dspec this
-
-# pylint: disable=return-outside-function,undefined-variable
-
-print(args[0])  # pylint: disable=undefined-variable  # type: ignore
