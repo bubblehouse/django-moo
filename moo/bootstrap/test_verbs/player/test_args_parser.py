61b36c4b3381bb5677816f5c8104671b9010b3d2
Phil Christensen <phil@bubblehouse.org>
2026-03-11 14:40:05 -0400

    test: remove default verbs from test dataset

--- moo/bootstrap/test_verbs/player/test_args_parser.py
+++
@@ -1,8 +0,0 @@
-#!moo verb test-args-parser --on "player class" --dspec none
-
-# pylint: disable=return-outside-function,undefined-variable
-
-from moo.core import context
-
-if context.parser is not None:
-    print("PARSER")
--- moo/bootstrap/test_verbs/player/test_nested_verbs.py
+++
@@ -1,8 +0,0 @@
-#!moo verb test-nested-verbs test_nested_verbs --on "player class" --dspec none
-
-# pylint: disable=return-outside-function,undefined-variable
-
-from moo.core import context
-
-print(1)
-context.caller.invoke_verb("test-nested-verbs-method", 1)
