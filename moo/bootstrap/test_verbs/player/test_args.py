baa7a3e5adff7f9d20f961a029d27e4e8ff2487e
Phil Christensen <phil@bubblehouse.org>
2026-03-11 14:40:05 -0400

    test: remove default verbs from test dataset

--- moo/bootstrap/test_verbs/player/test_args.py
+++
@@ -1,8 +0,0 @@
-#!moo verb test-args test_args --on "player class" --dspec this
-
-# pylint: disable=return-outside-function,undefined-variable
-
-from moo.core import context
-
-if args is not None:  # pylint: disable=undefined-variable
-    print(f"METHOD:{args}:{kwargs}")  # pylint: disable=undefined-variable
