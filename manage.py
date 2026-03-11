436a3a703499034098b390bba6e5a0509987ddbf
Phil Christensen <phil@bubblehouse.org>
2026-03-11 10:01:58 -0400

    test: wire up open telemetry for local development

--- manage.py
+++ manage.py
@@ -3,12 +3,24 @@
 import os
 import sys
 
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.instrumentation.django import DjangoInstrumentor
+from opentelemetry.instrumentation.psycopg2 import Psycopg2Instrumentor
+from opentelemetry.instrumentation.redis import RedisInstrumentor
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "moo.settings")
+    provider = TracerProvider(resource=Resource.create())
+    provider.add_span_processor(BatchSpanProcessor(OTLPSpanExporter()))
+    trace.set_tracer_provider(provider)
     DjangoInstrumentor().instrument()
+    Psycopg2Instrumentor().instrument()
+    RedisInstrumentor().instrument()
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
