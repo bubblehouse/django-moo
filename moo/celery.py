8cbaea90c95ac3cf8225c5aa6b86762dca2b3ce6
Phil Christensen <phil@bubblehouse.org>
2026-03-11 10:01:58 -0400

    test: wire up open telemetry for local development

--- moo/celery.py
+++ moo/celery.py
@@ -9,7 +9,14 @@ from celery import Celery, bootsteps
 from celery.signals import setup_logging, beat_init, worker_ready, worker_shutdown, worker_process_init
 from kombu.serialization import register
 
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.instrumentation.celery import CeleryInstrumentor
+from opentelemetry.instrumentation.psycopg2 import Psycopg2Instrumentor
+from opentelemetry.instrumentation.redis import RedisInstrumentor
 
 from . import celeryconfig
 from .core import moojson
@@ -27,7 +34,12 @@ register("moojson", moojson.dumps, moojson.loads, content_type="application/x-mo
 
 @worker_process_init.connect(weak=False)
 def init_celery_tracing(*args, **kwargs):
+    provider = TracerProvider(resource=Resource.create())
+    provider.add_span_processor(BatchSpanProcessor(OTLPSpanExporter()))
+    trace.set_tracer_provider(provider)
     CeleryInstrumentor().instrument()
+    Psycopg2Instrumentor().instrument()
+    RedisInstrumentor().instrument()
 
 @setup_logging.connect
 def configure_celery_logging(**kwargs):
