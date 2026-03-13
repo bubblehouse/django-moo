d4bc30357a734dee8c0c01fdb03a6c26d44753f3
Phil Christensen <phil@bubblehouse.org>
2026-03-13 12:20:13 -0400

    feat: use the redis result backend when not in testing

--- moo/settings/test.py
+++ moo/settings/test.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
+import os
+
 from .base import *  # pylint: disable=wildcard-import
+
+os.environ.setdefault("CELERY_RESULT_BACKEND", "django-db")
+
+INSTALLED_APPS += ["django_celery_results"]  # noqa: F405
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-061+p62f39ohlfrgu&)%1lxo%%#_-$rc5l_zsrlx6jqy)sw(=r"
