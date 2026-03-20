59676b8bc56c3537cd154bf6ce17f10efb40e5bd
Phil Christensen <phil@bubblehouse.org>
2026-03-20 14:15:33 -0400

    feat: support `obvious` on objects

---
+++ moo/core/migrations/0027_object_obvious_default_false.py
@@ -0,0 +1,16 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("core", "0026_remove_task_origin_remove_task_user_and_more"),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name="object",
+            name="obvious",
+            field=models.BooleanField(default=False),
+        ),
+    ]
