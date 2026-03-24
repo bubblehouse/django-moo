68297ecfc2489a40b987edd07d76d5249e8394f2
Phil Christensen <phil@bubblehouse.org>
2026-03-24 00:07:39 -0400

    feat: merged webssh index.html into django for foture expansion

--- moo/urls.py
+++ moo/urls.py
@@ -8,6 +8,9 @@ See https://docs.djangoproject.com/en/stable/topics/http/urls/ for details.
 from django.contrib import admin
 from django.urls import path
 
+from moo.shell.views import terminal
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", terminal, name="terminal"),
 ]
