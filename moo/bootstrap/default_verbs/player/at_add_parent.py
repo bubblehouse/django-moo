cdcd1c8dfd46c1aa96b0e4771295e2a118e61cd3
Phil Christensen <phil@bubblehouse.org>
2026-04-06 07:09:51 -0400

    fix: improved verb handling

--- moo/bootstrap/default_verbs/player/at_add_parent.py
+++ moo/bootstrap/default_verbs/player/at_add_parent.py
@@ -3,11 +3,11 @@
 # pylint: disable=return-outside-function,undefined-variable
 
 """
-Add a parent class to an object. The direct object is the object to modify;
-the indirect object (after "to") is the parent class to add.
+Add a parent class to an object. The direct object is the parent to add;
+the indirect object (after "to") is the object to modify.
 
-Usage: @add_parent #N to "$furniture"
-       @add_parent "oak desk" to "$container"
+Usage: @add_parent "$furniture" to #N
+       @add_parent "$container" to "oak desk"
 
 Requires write permission on the object and derive permission on the parent.
 """
@@ -15,7 +15,7 @@ Requires write permission on the object and derive permission on the parent.
 from moo.sdk import context, lookup
 
 parser = context.parser
-obj = parser.get_dobj()
-parent = parser.get_pobj("to", lookup=True)
+parent = parser.get_dobj(lookup=True)
+obj = parser.get_pobj("to", lookup=True)
 obj.add_parent(parent)
 print(f"Added {parent} as a parent of {obj}.")
