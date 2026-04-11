1b9515a59f590e8cde15c31f131a1c2c8182d028
Phil Christensen <phil@bubblehouse.org>
2026-04-11 11:54:16 -0400

    fix(verbs): fix set_opaque property name, opacity import, open locked message, listgag tell output

--- moo/bootstrap/default_verbs/container/at_opacity.py
+++ moo/bootstrap/default_verbs/container/at_opacity.py
@@ -20,6 +20,8 @@ The syntax for ``@opacity`` is:
 where '#' is either 0, 1 or 2.
 """
 
+from moo.sdk import context
+
 opacity = args[0] if args else context.parser.get_pobj_str("is")
 if opacity not in ("0", "1", "2"):
     raise ValueError("Opacity must be 0, 1 or 2.")
