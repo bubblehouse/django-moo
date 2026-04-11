16dcbeb53e85f21f2549bded697d7101c3497d4b
Phil Christensen <phil@bubblehouse.org>
2026-04-11 12:11:14 -0400

    feat(core): add site FK to Object and Player models

--- moo/core/models/auth.py
+++ moo/core/models/auth.py
@@ -9,10 +9,12 @@ from django.db import models
 
 class Player(models.Model):
     class Meta:
+        unique_together = [["user", "site"]]
         indexes = [
             models.Index(fields=["avatar", "wizard"], name="player_avatar_wizard_idx"),
         ]
 
-    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
     avatar = models.ForeignKey("Object", null=True, on_delete=models.SET_NULL)
     wizard = models.BooleanField(default=False)
+    site = models.ForeignKey("sites.Site", null=True, blank=True, on_delete=models.SET_NULL)
