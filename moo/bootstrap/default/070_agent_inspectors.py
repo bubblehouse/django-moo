b5df2e413787ae0fd7eae0d4a9f45b63b6d8762c
Phil Christensen <phil@bubblehouse.org>
2026-04-13 19:21:24 -0400

    chore(agents): remove unused Tailor inspector agent

--- moo/bootstrap/default/070_agent_inspectors.py
+++ moo/bootstrap/default/070_agent_inspectors.py
@@ -43,14 +43,3 @@ if archivist_created:
     archivist_user.set_password("Wr7mK5pDcF3n")
     archivist_user.save()
 Player.objects.get_or_create(user=archivist_user, defaults=dict(avatar=archivist_obj))
-
-# Tailor ($player): exercises @gender, pronoun substitution, @messages, @check
-tailor_obj, _ = bootstrap.get_or_create_object("Tailor", unique_name=True, parents=[player])
-tailor_obj.owner = tailor_obj
-tailor_obj.set_property("home", agency)
-tailor_obj.save()
-tailor_user, tailor_created = User.objects.get_or_create(username="tailor")
-if tailor_created:
-    tailor_user.set_password("Gv2sM9xJnH6q")
-    tailor_user.save()
-Player.objects.get_or_create(user=tailor_user, defaults=dict(avatar=tailor_obj))
